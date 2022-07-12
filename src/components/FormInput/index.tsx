d0699044e9f2a21de61955ae496f664cfaf92073
sandy <BraveShining@gmail.com>
2022-07-12 22:42:47 +0800

    modify: page6 style

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,44 +1,66 @@
-import { CSSProperties } from '@material-ui/core/styles/withStyles'
-import './style.css'
+import { CSSProperties } from "@material-ui/core/styles/withStyles";
+import "./style.css";
 
 type propsTyle = {
-    title: string,
-    placeholder: string,
-    index: number,
-    width?: string,
-    height?: string,
-    isTextArea?: boolean,
-    formInfo?: any,
-    setFormInfo?: any,
-
-}
+  title: string;
+  placeholder: string;
+  index: number;
+  width?: string;
+  height?: string;
+  isTextArea?: boolean;
+  formInfo?: any;
+  setFormInfo?: any;
+};
 
 const Index = (props: propsTyle) => {
+  const {
+    title,
+    placeholder,
+    width = "100%",
+    height = "80px",
+    isTextArea = false,
+    formInfo,
+    setFormInfo,
+    index,
+  } = props;
 
-    const { title, placeholder, width = '480px', height = '80px', isTextArea = false, formInfo, setFormInfo, index } = props;
+  const titleStyle: CSSProperties = {
+    fontSize: "2.3rem",
+    fontFamily: "OPPOSans",
+    color: "#000000",
+    marginBottom: "12px",
+  };
 
-    const titleStyle: CSSProperties = {
-        fontSize: '30px',
-        fontFamily: 'OPPOSans',
-        color: '#000000',
-        marginBottom: '12px'
-    }
+  const handleChange = (e: any) => {
+    const arr = [...formInfo];
+    arr[index] = e.target.value;
+    setFormInfo(arr);
+  };
 
-    const handleChange = (e: any) => {
-        const arr = [...formInfo];
-        arr[index] = e.target.value;
-        setFormInfo(arr);
-    }
+  return (
+    <div>
+      <div className="inputTitle" style={titleStyle}>
+        {title}
+      </div>
+      {!isTextArea ? (
+        <input
+          className="inputRect"
+          placeholder={placeholder}
+          style={{  height, right: 0 }}
+          value={formInfo[index]}
+          onChange={handleChange}
+        ></input>
+      ) : (
+        <textarea
+          className="inputRect"
+          placeholder={placeholder}
+          style={{ width, height, resize: "none" }}
+          value={formInfo[index]}
+          onChange={handleChange}
+        ></textarea>
+      )}
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            <div className="inputTitle" style={titleStyle}>{title}</div>
-            {!isTextArea ? <input className="inputRect" placeholder={placeholder} style={{ width, height }} value={formInfo[index]} onChange={handleChange}></input>
-                : <textarea className="inputRect" placeholder={placeholder} style={{ width, height, resize: 'none' }} value={formInfo[index]} onChange={handleChange}></textarea>
-            }
-        </div>
-    )
-}
-
-export { Index as FormInput }
+export { Index as FormInput };
--- src/contianer/Page6/index.tsx
+++ src/contianer/Page6/index.tsx
@@ -1,91 +1,127 @@
-import { Button } from "@material-ui/core"
-import { CSSProperties } from "@material-ui/core/styles/withStyles"
-import { useState } from "react"
-import { FormInput } from "../../components/FormInput"
-import './style.css'
+import { Button } from "@material-ui/core";
+import { CSSProperties } from "@material-ui/core/styles/withStyles";
+import { useState } from "react";
+import { FormInput } from "../../components/FormInput";
+import "./style.css";
 
 const flexStyle: CSSProperties = {
-    width: '1912px',
-    height: `${5000 - 80}px`,
-    display: 'flex',
-    flexDirection: 'column',
-}
-
-
+  width: "1912px",
+  height: `${5000 - 80}px`,
+  display: "flex",
+  flexDirection: "column",
+};
 
 const formStyle: CSSProperties = {
-    backgroundColor: '#EAEAEA',
-    width: '100%',
-    height: '100%',
-}
+  backgroundColor: "#EAEAEA",
+  width: "100%",
+  height: "100%",
+};
 
 //提交表单 css
 const formFlexStyle: CSSProperties = {
-    display: 'flex',
-    flexWrap: 'wrap',
-    width: '1560px',
-    marginLeft: '180px',
-    justifyContent: 'space-between',
-    marginTop: '120px'
-
-}
-
-
+  display: "flex",
+  flexWrap: "wrap",
+  width: "83%",
+//   marginLeft: "7%",
+  marginRight: "10%",
+  justifyContent: "space-between",
+  marginTop: "120px",
+  paddingLeft: "7%",
+};
 
 const Index = () => {
-    const [formInfo, setFormInfo] = useState(['', '', '', ''])
+  const [formInfo, setFormInfo] = useState(["", "", "", ""]);
 
-    const handleSubmit = () => {
-        console.log(formInfo);
-        //fetch todo...
-    }
+  const handleSubmit = () => {
+    console.log(formInfo);
+    //fetch todo...
+  };
 
-    return (
-        // <div style={flexStyle}>
-        <div className="box">
-            <div >
-                <img src="/page6/page6_banner.jpg" />
-            </div>
-            <div style={formStyle}>
-                <div className="title">
-                    服务客户
-                </div>
-                <div className="form-title">
-                    在线提交您的需求
-                </div>
-                <div className="">
-                    <div style={formFlexStyle}>
-                        <FormInput title={"您的姓名:"} placeholder={"请输入您的名字"} formInfo={formInfo} setFormInfo={setFormInfo} index={0} />
-                        <FormInput title={"您的电话/手机:"} placeholder={"请输入您的电话/手机"} formInfo={formInfo} setFormInfo={setFormInfo} index={1} />
-                        <FormInput title={"您的邮箱:"} placeholder={"请输入您的邮箱"} formInfo={formInfo} setFormInfo={setFormInfo} index={2} />
-                        <div style={{ marginTop: '80px' }}>
-                            <FormInput title={"填写您的需求:"} placeholder={"请输入您的需求"} width={'1560px'} height={'360px'} isTextArea={true} formInfo={formInfo} setFormInfo={setFormInfo} index={3} />
-                        </div>
-                    </div>
-                    <div style={{ marginTop: '40px' }}>
-                        <Button style={{
-                            width: '241px',
-                            height: '80px',
-                            background: '#000000',
-                            borderRadius: '5px',
-                            color: '#ffffff',
-                            marginLeft: '840px',
-                            marginTop: '40px',
-                            fontSize: '36px',
-                            fontFamily: 'OPPOSans'
-                        }} onClick={handleSubmit}>提交</Button >
-                    </div>
-                </div>
-            </div>
-            <div>
-                <img src="/page6/page6_p1.jpg" />
-            </div>
-            <div>
-                <img src="/page6/page6_bottom.jpg" />
-            </div>
+  const flexLayout = { height: "10vh", width: "30%" };
 
-        </div >
-    )
-}
+  const input = (index: number) => inputs[index];
 
-export { Index as Page6 }
+  const inputs = [
+    <FormInput
+      title={"您的姓名:"}
+      placeholder={"请输入您的名字"}
+      formInfo={formInfo}
+      setFormInfo={setFormInfo}
+      index={0}
+    />,
+    <FormInput
+      title={"您的电话/手机:"}
+      placeholder={"请输入您的电话/手机"}
+      formInfo={formInfo}
+      setFormInfo={setFormInfo}
+      index={1}
+    />,
+    <FormInput
+      title={"您的邮箱:"}
+      placeholder={"请输入您的邮箱"}
+      formInfo={formInfo}
+      setFormInfo={setFormInfo}
+      index={2}
+    />,
+  ];
+
+  return (
+    // <div style={flexStyle}>
+    <div className="box">
+      <div>
+        <img src="/page6/page6_banner.jpg" />
+      </div>
+      <div style={formStyle}>
+        <div className="title">服务客户</div>
+        <div className="form-title">在线提交您的需求</div>
+        <div className="">
+          <div style={formFlexStyle}>
+            <div style={flexLayout}>{input(0)}</div>
+            <div style={flexLayout}>{input(1)}</div>
+            <div style={flexLayout}>{input(2)}</div>
+          </div>
+          <div
+            style={{ marginTop: "80px", marginLeft: "7%", marginRight: "10%" }}
+          >
+            <FormInput
+              title={"填写您的需求:"}
+              placeholder={"请输入您的需求"}
+              width={"100%"}
+              height={"39vh"}
+              isTextArea={true}
+              formInfo={formInfo}
+              setFormInfo={setFormInfo}
+              index={3}
+            />
+          </div>
+          <div style={{ marginTop: "40px" }}>
+            <Button
+              style={{
+                width: "20%",
+                // height: "10%",
+                background: "#000000",
+                borderRadius: "5px",
+                color: "#ffffff",
+                marginLeft: "40%",
+                marginTop: "5vh",
+                fontSize: "36px",
+                fontFamily: "OPPOSans",
+              }}
+              onClick={handleSubmit}
+            >
+              提交
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src="/page6/page6_p1.jpg" />
+      </div>
+      <div>
+        <img src="/page6/page6_bottom.jpg" />
+      </div>
+    </div>
+  );
+};
+
+export { Index as Page6 };
